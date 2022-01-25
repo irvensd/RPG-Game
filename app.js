df95f4caf41ce8b1f23d182633efab2d08d4e155
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2022-01-25 17:00:28 -0500

    Game Logic

---
+++ app.js
@@ -0,0 +1,18 @@
+const Game = require('./lib/Game')
+
+Game.prototype.startNewBattle = function(){
+    if (this.player.agility > this.currentEnemy.agility) {
+        this.isPlayerTurn = true;
+    } else {
+        this.isPlayerTurn = false;
+    }
+
+    console.log('Your stats are as follows:');
+    console.table(this.player.getStats());
+
+    console.log(this.currentEnemy.getDescription());
+
+    this.battle()
+};
+
+new Game().initializeGame();
